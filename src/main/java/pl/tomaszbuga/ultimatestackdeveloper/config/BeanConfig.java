57048c1f2a6af2daa3bd90fe1265841e46ef6a75
kurular4 <kurular4@gmail.com>
2021-08-07 09:43:26 +0300

    Model mapper dependency added

--- src/main/java/pl/tomaszbuga/ultimatestackdeveloper/config/BeanConfig.java
+++ src/main/java/pl/tomaszbuga/ultimatestackdeveloper/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package pl.tomaszbuga.ultimatestackdeveloper.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,5 +12,10 @@ public class BeanConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
